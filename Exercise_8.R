5f1021e9e57553a87f8bae21f9b43d0bd8a8af62
Samantha Masca <samanthamasca@Samanthas-MacBook-Air.local>
2020-10-20 17:32:47 -0400

    Exercise 8 commit

--- Exercise_8.R
+++ Exercise_8.R
@@ -3,17 +3,24 @@
 # Exercise 8
 # TA: Elizabeth Brooks    
 
-# Task 1: generate a graph of the UW vs MSU game 
+# Task 1: generate a graph of the UW vs MSU game from 1/22/2013 
+
+# To do:
+# 1-load data
+# 2-set variables
+# 3-make new matrix of data
+# 4-plot data on graph
 
 # load data
 setwd("/Users/samanthamasca/Biocomputing2020_Tutorial10/")
 data <- read.table(file="UWvMSU_1-22-13.txt",header = TRUE, stringsAsFactors = FALSE)
 
+# set variables 
 sumUW <- 0
 sumMSU <- 0
 M <- matrix(0,nrow=3,ncol=0)
 
-# upload data 
+# upload data into new matrix (time, cumulative UW score, cumulative MSU score)
 for (i in 1:nrow(data)) {
   if (data$team[i]=="UW") {
     sumUW = sumUW + data$score[i]
@@ -23,34 +30,31 @@ for (i in 1:nrow(data)) {
   }
   M <- cbind(M, c(data$time[i],sumUW,sumMSU))  
 }
+rownames(M) <- c("Time", "UW", "MSU")
 
 # plot the graph 
-rownames(M) <- c("Time", "UW", "MSU")
-plot(M[1,],M[2,],type = 'l', xlab="Time", ylab="Score", col="red") # plot time vs. UW score
-lines(M[1,],M[3,],col="green") # plot time vs MSU score
+plot(M[1,],M[2,],type = 'l', xlab="Time", ylab="Score", col="red") # UW score = red line
+lines(M[1,],M[3,],col="green") # MSU score = green line
 
 ############################################################
 
 # Task 2: 'guess my number' game
 
-# choose random number 
+# To do:
+# 1-choose random number 
+# 2-play game (by using conditional while loop)
+
 num <- sample(1:100,1,replace=FALSE,prob=NULL)
-
-# play game
 cat("I'm thinking of a number 1-100...")
-guess <- readline(prompt = "Guess: ")
-
 while (TRUE) {
+  guess <- readline(prompt = "Guess: ")
   if (guess == num) {
     return(cat("Correct!\n"))
   }
   else if (guess > num) {
     cat("Lower\n")
-    guess <- readline(prompt = "Guess: ")
   }
   else if (guess < num) {
     cat("Higher\n")
-    guess <- readline(prompt = "Guess: ")
   }
 }
-
